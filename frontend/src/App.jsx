645ef0630d943a471402549ba60415783a0bc534
Abhishek Kapoor <ab210637@dal.ca>
2024-06-26 23:00:12 -0300

    Room Create, View done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>Dalhousie University</h1>
+        <button>Check Availability</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
