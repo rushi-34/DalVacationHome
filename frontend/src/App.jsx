1ec5591e4ce2b56e631bb9f5fa06e37c6423b955
Abhishek Kapoor <ab210637@dal.ca>
2024-06-28 18:14:49 -0300

    Make basic, barebone Login and Register pages. No backend functionality added.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import CheckAvailability from './pages/CheckAvailability';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/check-availability" element={<CheckAvailability />} />
       </Routes>
     </Router>
